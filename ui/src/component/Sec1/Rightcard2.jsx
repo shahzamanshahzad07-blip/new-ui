e9b65ea7a309fa236fde7485914b72a4fed26994
shahzamanshahzad07-blip <shahzamanshahzad07@gmail.com>
2025-12-03 00:25:47 -0700

    first commit

--- ui/src/component/Sec1/Rightcard2.jsx
+++ ui/src/component/Sec1/Rightcard2.jsx
@@ -5,7 +5,7 @@ const Rightcard2 = () => {
     <div className=' h-full shrink-0 overflow-hidden relative rounded-4xl  '>
 <img className='h-full  w-55  object-cover  ' src="https://thumbs.dreamstime.com/b/happy-new-year-background-bokeh-lights-suitable-greeting-card-modern-mobile-wallpaper-vector-illustration-299534292.jpg?w=768" alt="" />   
 <div className=' absolute top-0 left-0 h-full w-full justify-between flex p-5  flex-col ' ><h1 className=' text-2xl font-bold justify-center items-center flex  p-5 bg-amber-50 rounded-4xl h-5 w-5 '>2</h1></div>
-i
+
       
     </div>
   )
